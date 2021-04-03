d0c2f749d55e68d91a72e918ceb79aa9a2916e50
Omar Alsalahy <omaralsalahy@yandex.com>
2021-04-03 14:42:20 -0300

    Display a user's devices inside MainActivity.java

--- app/src/com/claroclient/MainActivity.java
+++ app/src/com/claroclient/MainActivity.java
@@ -2,18 +2,32 @@ package com.claroclient;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+
 
 import com.claroclient.AppRepository;
+import com.claroclient.DeviceAdapter;
 import com.claroclient.model.User;
+import com.claroclient.model.Device;
 
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
 
   private AppRepository repo;
   private TextView userIdText;
+  private DeviceAdapter adapter;
+  private RecyclerView recyclerView;
 
   /** Called when the activity is first created. */
   @Override
@@ -26,7 +40,25 @@ public class MainActivity extends AppCompatActivity
 
     String userId = repo.getUserId();
 
-    userIdText = (TextView) findViewById(R.id.userId);
+    recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+    userIdText   = (TextView) findViewById(R.id.userId);
+
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
     userIdText.setText(userId);
+
+    repo.getDeviceList(userId)
+      .subscribeOn(Schedulers.io())
+      .observeOn(AndroidSchedulers.mainThread())
+      .subscribe(this::handleSuccess, this::handleError);
+  }
+
+  private void handleSuccess(List<Device> devices){
+    adapter = new DeviceAdapter(this, devices);
+    recyclerView.setAdapter(adapter);
+  }
+
+  private void handleError(Throwable t){
+    Toast.makeText(this, "Failed to find devices", Toast.LENGTH_SHORT)
+      .show();
   }
 }
